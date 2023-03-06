254e5568a06144539fbb55657752dda117e73bce
Nikita Poznyak <valdefro10@gmail.com>
2023-03-06 11:18:06 +0300

    test commit

--- app/src/main/java/com/vladimir_tsurko/drugsstore/MainActivity.kt
+++ app/src/main/java/com/vladimir_tsurko/drugsstore/MainActivity.kt
@@ -2,6 +2,7 @@ package com.vladimir_tsurko.drugsstore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vladimir_tsurko.drugstore.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
