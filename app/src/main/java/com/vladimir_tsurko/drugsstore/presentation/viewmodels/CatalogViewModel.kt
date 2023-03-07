43263ff9fca5a3328fae02cb176d595903f9e5e5
Danilamaimeskul <dan.maika@mail.ru>
2023-03-07 18:46:31 +0300

    setup catalog viewmodel

---
+++ app/src/main/java/com/vladimir_tsurko/drugsstore/presentation/viewmodels/CatalogViewModel.kt
@@ -0,0 +1,32 @@
+package com.vladimir_tsurko.drugsstore.presentation.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.vladimir_tsurko.drugstore.data.remote.dto.productDto.ProductDto
+import com.vladimir_tsurko.drugstore.domain.usecases.GetAllProductsUseCase
+import com.vladimir_tsurko.drugstore.utils.Resource
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class CatalogViewModel @Inject constructor(
+    private val getAllProductsUseCase: GetAllProductsUseCase
+): ViewModel() {
+
+
+    private var _productsResponse = MutableLiveData<Resource<List<ProductDto>>?>()
+    val productsResponse: LiveData<Resource<List<ProductDto>>?>
+        get() = _productsResponse
+
+    init{
+        getAllProducts()
+    }
+
+
+    private fun getAllProducts() = viewModelScope.launch {
+        _productsResponse.value = getAllProductsUseCase()
+    }
+
+
+}
