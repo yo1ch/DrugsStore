25c990790d823c714e4ee81f502919b3c6f24252
Danilamaimeskul <dan.maika@mail.ru>
2023-03-07 16:38:51 +0300

    setup catalog fragment

---
+++ app/src/main/java/com/vladimir_tsurko/drugsstore/presentation/fragments/CatalogFragment.kt
@@ -0,0 +1,82 @@
+package com.vladimir_tsurko.drugsstore.presentation.fragments
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.vladimir_tsurko.App
+import com.vladimir_tsurko.drugstore.R
+import com.vladimir_tsurko.drugstore.databinding.FragmentCatalogBinding
+import com.vladimir_tsurko.drugstore.databinding.FragmentLoginBinding
+import com.vladimir_tsurko.drugstore.domain.models.LoginModel
+import com.vladimir_tsurko.drugstore.presentation.adapters.ProductListAdapter
+import com.vladimir_tsurko.drugstore.presentation.viewmodels.AuthViewModel
+import com.vladimir_tsurko.drugstore.presentation.viewmodels.CatalogViewModel
+import com.vladimir_tsurko.drugstore.presentation.viewmodels.ViewModelFactory
+import com.vladimir_tsurko.drugstore.utils.Resource
+import javax.inject.Inject
+
+class CatalogFragment : Fragment() {
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private lateinit var viewModel: CatalogViewModel
+
+    private val component by lazy{
+        (requireActivity().application as App).component
+    }
+
+    private var _binding: FragmentCatalogBinding? = null
+    private val binding: FragmentCatalogBinding
+        get() = _binding ?: throw RuntimeException("FragmentCatalogBinding == null")
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this, viewModelFactory)[CatalogViewModel::class.java]
+        setupRecyclerView()
+
+
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCatalogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onAttach(context: Context) {
+        component.inject(this)
+        super.onAttach(context)
+    }
+
+    private fun setupRecyclerView(){
+        val adapter = ProductListAdapter()
+        binding.rvProductsList.adapter = adapter
+        viewModel.productsResponse.observe(viewLifecycleOwner){
+            when(it) {
+                is Resource.Success ->{
+                    adapter.submitList(it.data)
+                }
+                is Resource.Error -> Toast.makeText(activity, it.message, Toast.LENGTH_SHORT).show()
+                is Resource.Loading -> {}
+                else -> {}
+            }
+        }
+
+    }
+
+
+}
