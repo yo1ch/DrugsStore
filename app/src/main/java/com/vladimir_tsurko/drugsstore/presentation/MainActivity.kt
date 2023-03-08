aee770089650ab8a1af06dd448cffb0aa8da8fd5
Volodya <volodechka00@gmail.com>
2023-03-09 02:59:50 +0300

    setup catalog screen toolbar

--- app/src/main/java/com/vladimir_tsurko/drugsstore/presentation/MainActivity.kt
+++ app/src/main/java/com/vladimir_tsurko/drugsstore/presentation/MainActivity.kt
@@ -2,6 +2,10 @@ package com.vladimir_tsurko.drugsstore.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.vladimir_tsurko.drugsstore.R
 
 
@@ -9,5 +13,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_bar)
+        val navController = findNavController(R.id.fragmentContainerView)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+
+    fun hideBottomBar(){
+        findViewById<BottomNavigationView>(R.id.bottom_navigation_bar).visibility = View.GONE
+    }
+
+    fun showBottomBar(){
+        findViewById<BottomNavigationView>(R.id.bottom_navigation_bar).visibility = View.VISIBLE
     }
 }
